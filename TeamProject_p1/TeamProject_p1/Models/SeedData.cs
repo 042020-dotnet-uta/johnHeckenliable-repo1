e833f0f6b139c4b084ced3d0d006ed24a946d408
Mibyhall <mhall1914@comcast.net>
2020-05-08 12:59:08 -0400

    adds seeder

---
+++ TeamProject_p1/TeamProject_p1/Models/SeedData.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
+using TeamProject_p1.Data;
+using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.CodeAnalysis;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace TeamProject_p1.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new ProjectDbContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<ProjectDbContext>>()))
+            {
+
+                // Look for any taskDates or tasks
+                if (context.TaskDates.Any() || context.Tasks.Any())
+                {
+                    return;
+                }
+
+                context.TaskDates.AddRange(
+                    new Calender
+                    {
+                        TaskDate = DateTime.Parse("2020-5-7")
+                    },
+
+                     new Calender
+                     {
+                         TaskDate = DateTime.Parse("2020-5-8")
+                     },
+
+                     new Calender
+                     {
+                         TaskDate = DateTime.Parse("2020-5-9")
+                     }
+                    );
+
+                context.Tasks.AddRange(
+                    new DailyTask
+                    {
+                        DateId = 1,
+                        Description = "Clean the dishes"
+                    },
+
+                    new DailyTask
+                    {
+                        DateId = 2,
+                        Description = "Finish this project"
+                    },
+
+                    new DailyTask
+                    {
+                        DateId = 3,
+                        Description = "Feed the dogs"
+                    }
+
+                );
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
