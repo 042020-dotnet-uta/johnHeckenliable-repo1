7a8654fce28f8893c1c59c32d1310326fef88068
jhecken <jdheckenliable@gmail.com>
2020-05-08 10:49:16 -0700

    Adds DBContext Methods

--- TeamProject_p1/TeamProject_p1/Data/ProjectDbContext.cs
+++ TeamProject_p1/TeamProject_p1/Data/ProjectDbContext.cs
@@ -15,9 +15,56 @@ namespace TeamProject_p1.Data
         public DbSet<DailyTask> DailyTasks { get; set; }
         public DbSet<Calendar> CalendarDates { get; set; }
 
-        public void AddTask()
+        public void AddNewTask(DailyTask dailyTask)
         {
-            
+            this.Add(dailyTask);
+            this.SaveChanges();
+        }
+
+        public DailyTask GetTaskDetails(int id)
+        {
+            var dailyTask = this.DailyTasks
+                .AsNoTracking()
+                .FirstOrDefault(m => m.DailyTaskId == id);
+
+            return dailyTask;
+        }
+        public IEnumerable<DailyTask> GetAllTasks()
+        {
+            var tasks = this.DailyTasks.AsNoTracking().ToList();
+            return tasks;
+        }
+
+        public void DeleteDailyTask(int id)
+        {
+            var dailyTask = this.DailyTasks.Find(id);
+            this.DailyTasks.Remove(dailyTask);
+            this.SaveChanges();
+        }
+
+        public void UpdateDailyTask(DailyTask dailyTask)
+        {
+            try
+            {
+                this.Update(dailyTask);
+                this.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DailyTaskExists(dailyTask.DailyTaskId))
+                {
+                    AddNewTask(dailyTask);
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+        private bool DailyTaskExists(int id)
+        {
+            return this.DailyTasks.Any(e => e.DailyTaskId == id);
+
         }
     }
 }
