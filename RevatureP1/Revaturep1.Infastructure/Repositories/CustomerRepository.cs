6d7d1803b0c23d78367141babea169e731dea238
jhecken <jdheckenliable@gmail.com>
2020-05-08 21:34:46 -0700

    InitialCreate of RevatureP1 solution and assc projects

---
+++ RevatureP1/Revaturep1.Infastructure/Repositories/CustomerRepository.cs
@@ -0,0 +1,27 @@
+﻿using RevatureP1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Revaturep1.Infastructure.Repositories
+{
+    public class CustomerRepository : GenericRepository<Customer>
+    {
+        public CustomerRepository(ShoppingDbContext context) : base(context)
+        {
+        }
+
+        public override Customer Update(Customer entity)
+        {
+            var customer = _context.Customers
+                .Single(c => c.CustomerId == entity.CustomerId);
+
+            customer.FirstName = entity.FirstName;
+            customer.LastName = entity.LastName;
+            customer.Email = entity.Email;
+
+            return base.Update(customer);
+        }
+    }
+}
---
+++ RevatureP1/Revaturep1.Infastructure/Repositories/ProductRepository.cs
@@ -0,0 +1,23 @@
+﻿using RevatureP1.Models;
+using System.Linq;
+
+namespace Revaturep1.Infastructure.Repositories
+{
+    public class ProductRepository : GenericRepository<Product>
+    {
+        public ProductRepository(ShoppingDbContext context) : base(context)
+        {
+        }
+
+        public override Product Update(Product entity)
+        {
+            var product = _context.Products
+                .Single(p => p.PoductId == entity.PoductId);
+
+            product.ProductDescription = entity.ProductDescription;
+            product.Price = entity.Price;
+
+            return base.Update(product);
+        }
+    }
+}
